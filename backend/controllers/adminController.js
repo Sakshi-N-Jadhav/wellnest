516fe4c8d3d9c1ba67c12c66ad8d4303a7c5dc1f
Sakshi-N-Jadhav <sakshij.150722@gmail.com>
2025-02-04 17:54:17 -0500

    modified:   backend/controllers/adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,14 +1,40 @@
+import validator from "validator"
+import bcrypt from 'bcrypt'
+import {v2 as cloudinary} from "cloudinary"
 //API for addind doctor
 
 const addDoctor = async(req,res) => {
     try{
         const{name, email,password,speciality,degree,experience,about,fees,address} = req.body
         const imageFile = req.file
-        console.log({name, email,password,speciality,degree,experience,about,fees,address},imageFile);
-    }
+        // console.log({name, email,password,speciality,degree,experience,about,fees,address},imageFile);
 
-    catch(error)
-    {}
+        //checking for all data to add doctor
+        if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address )
+        {
+            return res.json({success:false, message:"Missing Details"})
+        }
+       //validating email format
+       if(!validator.isEmail(email)){
+        return res.json({success:false, message:"Please enter a valid email"})
+       }
+
+       //validating strong password
+       if(password.length<8)
+       {
+        return res.json({success:false, message:"Please enter a strong email"})
+       }
+        //hashing doctor password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
     
+
+    }
+    
+    catch(error){}
+
+   
+
 }
+
 export {addDoctor}  
